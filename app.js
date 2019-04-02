0bd0b1c546f833393d447a7ce9c061854c3a7d0d
newcaptain <newcaptain@foxmail.com>
2019-04-02 11:07:57 +0800

    完善服务端一些功能

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const app = new Koa();
 const server = require('http').Server(app.callback());
 const io = require('socket.io')(server);
 
-server.listen(8888, ()=> {
-  console.log('服务器已经打开');
-})
+const PORT = 8888;
+
+server.listen(PORT, ()=> {
+  console.log('服务器已经打开, 正在监听' + PORT + '端口');
+});
 
 io.on('connection', function (socket) {
+  console.log(socket.handshake.address + '连接了');
   socket.on('send', (data) => {
     io.sockets.emit('hasMsg',{user: socket.handshake.address, msg: data});
+  });
+  socket.on('disconnect', () => {
+    console.log(socket.handshake.address + '断开了连接');
   })
-  console.log(socket.handshake.address + '连接了');
 });
 
 app.use(serve('.'));
