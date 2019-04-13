4d948881f4e4e84a09c79f6aa92c2e39de606428
newcaptain <newcaptain@foxmail.com>
2019-04-13 12:25:17 +0800

    更改目录结构，引入koa-router、koa-bodyparser、添加登录注册页面

--- server/app.js
+++ server/app.js
@@ -1,17 +1,21 @@
 const Koa = require('koa');
 const serve = require('koa-static');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
+const router = new Router();
 const app = new Koa();
+
 const server = require('http').Server(app.callback());
 const io = require('socket.io')(server);
 
 const PORT = 8888;
 
-server.listen(PORT, ()=> {
+server.listen(PORT, () => {
   console.log('服务器已经打开, 正在监听' + PORT + '端口');
 });
 
-io.on('connection', function (socket) {
+io.on('connection', socket => {
   console.log(socket.handshake.address + '连接了');
   socket.on('send', (data) => {
     io.sockets.emit('hasMsg',{user: socket.handshake.address, msg: data});
@@ -21,4 +25,18 @@ io.on('connection', function (socket) {
   })
 });
 
-app.use(serve('.'));
+// 配置body解析
+app.use(bodyParser());
+
+// 静态资源托管
+app.use(serve(__dirname + './../static'));
+
+router.post('/login', (ctx, next) => {
+  console.log(ctx.request.body);
+});
+
+// 使用koa-router路由
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
