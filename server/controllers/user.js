a97c0a1660f03251764b43f6d6826f2e6a348678
newcaptain <newcaptain@foxmail.com>
2019-04-14 23:44:27 +0800

    解决路由找不到问题

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,7 @@
-
-
 module.exports = {
   async login(ctx) {
     console.log('userController');
+    ctx.body = 'userCotroller';
   },
 
   async register(ctx) {
--- server/routers/user.js
+++ server/routers/user.js
@@ -3,7 +3,7 @@ const router = require('koa-router')();
 const userController = require('./../controllers/user');
 
 const routers = router
-  .post('/user/login', userController.login)
-  .post('/user/register', userController.register);
+  .post('/login', userController.login)
+  .post('/register', userController.register);
 
   module.exports = routers;
